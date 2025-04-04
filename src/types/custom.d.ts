f8761db1184643adee62b6a47cfa8a0e145e2185
DevSA-009 <safin.ali.7205@gmail.com>
2025-04-05 04:02:37 +0600

    refactor: :label: new types ,modified utils types and missed types fixed

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -2,19 +2,21 @@ type findElementCb<T> = (element: T, index?: number) => boolean;
 interface RowInfo {
     height: number;
     remaining: number;
-    fitIn: number;
+    y: number;
+    x: number;
+    remainingStartIndex: number;
 }
 
 interface RowInfoReturn {
-    rowIn0:RowInfo;
-    rowIn90:RowInfo;
-    recommendedIn90:boolean;
+    rowIn0: RowInfo;
+    rowIn90: RowInfo;
+    recommendedIn90: boolean;
 }
 
 interface OrgBodyItem {
-    item:PageItem;
-    x:number;
-    y:number;
+    item: PageItem;
+    x: number;
+    y: number;
 }
 
 type Selection = PageItem[];
@@ -25,28 +27,40 @@ type PrevNextItems = {
     next: PageItem | null;
 };
 
-type MensSize = keyof typeof JFT_SIZE.MENS;
-type BabySize = keyof typeof JFT_SIZE.BABY;
+type MensSize = "S" | "M" | "L" | "XL" | "2XL";
+type BabySize = "2" | "4" | "6" | "8" | "10" | "12" | "14" | "16";
 
 type Mode = "PANT" | "B" | "FB";
 
 interface OrganizeInitParams {
     doc: Application["activeDocument"];
-    quantity:number;
-    mode:Mode
-    targetSizeChr:MensSize | BabySize;
+    quantity: number;
+    mode: Mode;
+    sizeConatiner?: string;
+    targetSizeChr: MensSize | BabySize;
+}
+
+interface CalculateDocRowDistributionParams {
+    dim: DimensionObject;
+    rowLength: number;
+    to90?:boolean;
+}
+interface CalculateDocRowDistributionReturn {
+    docNeed:number;
+    perDocRow:number;
 }
 
 interface OrgBodyItemDir {
-    item:PageItem;
-    quantity:number;
-    fitIn:number;
-    to90:boolean;
+    items: Selection;
+    mode: Mode;
+    quantity: number;
+    fitIn: number;
+    to90: boolean;
 }
 
-interface ListData {
-    readonly number:number;
-    readonly name:string;
+interface Person {
+    readonly number: number;
+    readonly name: string;
 }
 
 type BasePositions = "L" | "R" | "T" | "B";
@@ -58,20 +72,48 @@ type RunFunctionParams = ((doc: Application["activeDocument"]) => {}) | (() => {
 type AlignPosition = BasePositions | "LC" | "RC" | "TC" | "BC" | "C" | "CX" | "CY";
 
 interface MoveItemAfterParams {
-    base:Selection | PageItem;
-    moving:PageItem;
-    gap?:number;
+    base: Selection | PageItem;
+    moving: PageItem;
+    gap?: number;
     position: BasePositions
 }
 
 interface FixOrganizeRotateAlignParams {
-    to90:boolean;
+    to90: boolean;
     lastItem: GroupItem;
-    baseItem:GroupItem;
+    baseItem: GroupItem;
 }
 
-interface OrgFBModeParams {
+type TempDocumentHandlerCB = (tempDoc: IllustratorDocument) => void
+
+type DataListConatiner = {
+    [key in MensSize | BabySize]: Person[];
+};
+
+type BabySizeCategory = {
+    [key in BabySize]: DimensionObject
 }
 
-type BoundsObject = { left: number; top: number; right: number; bottom: number}
+type MensSizeCategory = {
+    [key in MensSize]: DimensionObject
+}
+
+interface SizeCategory {
+    "MENS": MensSizeCategory;
+    "BABY": BabySizeCategory;
+}
+
+interface SizeContainer {
+    [key: string]: SizeCategory
+}
+
+interface OrganizerParams {
+    dataString: string;
+    mode: Mode;
+    targetSizeChr: MensSize | BabySize;
+    doc?: Document;
+    sizeConatiner: string;
+}
+
+type BoundsObject = { left: number; top: number; right: number; bottom: number }
 type DimensionObject = { width: number; height: number; }
