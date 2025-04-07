442c0556f10fb8fb367facf3401ba0696bdd3239
DevSA-009 <safin.ali.7205@gmail.com>
2025-04-07 12:40:59 +0600

    refactor: :art: created `enum` for PageItemType

---
+++ src/enum/enums.ts
@@ -0,0 +1,8 @@
+enum PageItemType {
+    TextFrame = "TextFrame",
+    PathItem = "PathItem",
+    GroupItem = "GroupItem",
+    CompoundPathItem = "CompoundPathItem",
+    PlacedItem = "PlacedItem",
+    RasterItem = "RasterItem",
+}
