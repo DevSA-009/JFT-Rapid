7e9a0255800902bcd63d6732b2a8f8cd44bf3f9c
DevSA-009 <safin.ali.7205@gmail.com>
2025-06-13 07:56:40 +0600

    refactor: :wastebasket: lShapeOnce prop removed

--- src/class/ItemsInitiater.ts
+++ src/class/ItemsInitiater.ts
@@ -4,14 +4,12 @@ class ItemsInitiater {
     private bodyItems: BodyItems;
     private targetSizeChr: ApparelSize;
     private dimension: DimensionObject;
-    private isLShapeOnce:boolean = false;
     private gap = CONFIG.Items_Gap * 72;
 
     constructor(params: ItemsInitiaterConst) {
         this.mode = params.mode;
         this.bodyItems = params.bodyItems;
         this.orientation = params.orientation;
-        this.isLShapeOnce = params.isLShapeOnce || this.isLShapeOnce;
         this.dimension = params.dimension;
         this.targetSizeChr = params.targetSizeChr;
         this.resizeBody();
@@ -21,8 +19,8 @@ class ItemsInitiater {
      * Initiates items in the configured layout orientation
      * @returns {GroupItem} The grouped items in specified layout
      */
-    public initiatedItem (): GroupItem {
-        if(this.orientation === "L") {
+    public initiatedItem(): GroupItem {
+        if (this.orientation === "L") {
             return this.initiateInL()!;
         } else if (this.orientation === "H") {
             return this.initiateInH();
@@ -48,10 +46,10 @@ class ItemsInitiater {
      * @private
      * @returns {GroupItem} Vertically stacked group
      */
-    private initiateInV (): GroupItem {
+    private initiateInV(): GroupItem {
         const [front, back] = this.bodyItems;
 
-        if(this.mode === "FB") {
+        if (this.mode === "FB") {
             moveItemAfter({
                 base: front,
                 moving: back,
@@ -70,26 +68,26 @@ class ItemsInitiater {
      * @private
      * @returns {GroupItem} Horizontally arranged group
      */
-    private initiateInH (): GroupItem {
+    private initiateInH(): GroupItem {
         const [front, back] = this.bodyItems;
 
-        rotateItems(this.bodyItems,-90);
+        rotateItems(this.bodyItems, -90);
 
-        if(this.mode === "FB") {
+        if (this.mode === "FB") {
             moveItemAfter({
                 base: front,
                 moving: back,
                 position: "R",
                 gap: this.gap
             });
-            rotateItems(back,180);
+            rotateItems(back, 180);
         };
 
         const groupManager = new GroupManager(this.bodyItems);
         groupManager.group();
         return groupManager.tempGroup!;
     };
-    
+
     /**
      * Creates L-shaped layout with mirrored duplicates
      * @private
@@ -106,33 +104,31 @@ class ItemsInitiater {
             base: front,
             moving: back,
             position: "R",
-            gap:this.gap
+            gap: this.gap
         });
 
         // step 2 reflecting
         let duplicated = [] as Selection;
 
-        if(!this.isLShapeOnce) {
-            duplicated = [front.duplicate(), back.duplicate()];
+        duplicated = [front.duplicate(), back.duplicate()];
 
-            rotateItems(duplicated, 180);
+        rotateItems(duplicated, 180);
 
-            moveItemAfter({
-                base: front,
-                moving: duplicated[1],
-                position: "T",
-                gap: this.gap
-            });
+        moveItemAfter({
+            base: front,
+            moving: duplicated[1],
+            position: "T",
+            gap: this.gap
+        });
 
-            moveItemAfter({
-                base: back,
-                moving: duplicated[0],
-                position: "T",
-                gap: this.gap
-            });
-        }
+        moveItemAfter({
+            base: back,
+            moving: duplicated[0],
+            position: "T",
+            gap: this.gap
+        });
 
-        const groupManager = new GroupManager([...this.bodyItems,...duplicated]);
+        const groupManager = new GroupManager([...this.bodyItems, ...duplicated]);
         groupManager.group();
 
         return groupManager.tempGroup!;
@@ -141,7 +137,6 @@ class ItemsInitiater {
 
 interface ItemsInitiaterConst {
     mode: Mode;
-    isLShapeOnce?:boolean
     orientation: LayoutShapeConstants;
     bodyItems: BodyItems;
     targetSizeChr: ApparelSize;
