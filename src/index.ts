78d966d60486441e83dd28de8dad1644fcade069
DevSA-009 <safin.ali.7205@gmail.com>
2025-06-13 22:02:52 +0600

    refactor: :art: have testing func and prod usable func for only now `gridMenuallyCB`

--- src/index.ts
+++ src/index.ts
@@ -4,47 +4,50 @@
 //@include './utils/utils.js';
 //@include './class/JSONFileHandler.js';
 //@include './class/IllustratorDocHandler.js';
+//@include './class/ItemsInitiater.js';
 //@include './class/ArtboardManager.js';
+//@include './class/AutomateGridLayout.js';
 //@include './class/GroupManager.js';
+//@include './class/GridLayoutInfo.js';
 //@include './class/Organizer.js';
-//@include './scriptUI/orgMenualInfoDialog.js';
+//@include './scriptUI/gridMenualInfoDialog.js';
 //@include './scriptUI/alertUI.js';
 
-const sizeContainerFetch = new JSONFileHandler("G:\\JFT-Khelar\\jft.conf").read();
 
-const CONFIG:JFTRapid_Config = {
-    Items_Gap:0.1,
-    PAPER_MAX_SIZE:63.25,
-    Size_Container:sizeContainerFetch as SizeContainer,
-    perDoc:0
+const sizeContainerFetch = new JSONFileHandler("G:\\JFT-Rapid\\jft.conf").read();
+
+const CONFIG: JFTRapid_Config = {
+    Items_Gap: 0.1,
+    orientation: "Auto",
+    sizeInc: 0,
+    PAPER_MAX_SIZE: 63.25,
+    Size_Container: sizeContainerFetch as SizeContainer,
+    perDoc: 1
 };
 
-const orgMenuallyCB = (params:OrgManuallyParams) => {
+const gridMenuallyCB = (params: OrgManuallyParams) => {
     try {
         const { mode, quantity, sizeContainer, targetSizeChr } = params;
 
-        const doc = app.activeDocument;
+        const dimension = Organizer.getBodyDimenstion({ sizeContainer, targetSizeChr });
 
-        const selection = doc.selection as Selection;
+        const filesSeqIndex = Math.abs(Organizer.getDirectoryFileInfo().nexFileIndex - 1);
 
-        const body = Organizer.getBody(doc, selection);
-
-        const tempDoc = Organizer.createTempDocument({
-            items: body,
-            mode,
-            cb: Organizer.selectBodyCB as TempDocumentHandlerParams["cb"]
-        });
-
-        Organizer.initializeOrganization({
-            doc: tempDoc.doc,
+        new AutomateGridLayout({
             mode,
             quantity,
+            dimension,
             targetSizeChr,
-            sizeContainer
-        });
-    } catch (error:any) {
+            filesSeqIndex
+        })
+
+    } catch (error: any) {
         alertDialogSA(error.message);
     }
 };
-
-orgMenualInfoDialog()
+// gridMenuallyCB({
+//     mode:"B",
+//     quantity:5,
+//     sizeContainer:"JFT",
+//     targetSizeChr:"XL"
+// })
