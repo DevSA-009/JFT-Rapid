93af64743e9ad8ecfdc718bebd1aa2bcc936de1b
DevSA-009 <safin.ali.7205@gmail.com>
2025-06-15 00:34:45 +0600

    perf: :package: compile src codes into single `script.js` and update `manifiest` for communicate with CEP

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-//@include './polyfill/es6-method.js';
-//@include './enum/enums.js';
-//@include './polyfill/json2.js';
-//@include './utils/utils.js';
-//@include './class/JSONFileHandler.js';
-//@include './class/IllustratorDocHandler.js';
-//@include './class/ItemsInitiater.js';
-//@include './class/ArtboardManager.js';
-//@include './class/AutomateGridLayout.js';
-//@include './class/ValidatorManager.js';
-//@include './class/GroupManager.js';
-//@include './class/GridLayoutInfo.js';
-//@include './class/Organizer.js';
-//@include './scriptUI/gridMenualInfoDialog.js';
-//@include './scriptUI/alertUI.js';
 
 
 const sizeContainerFetch = new JSONFileHandler("G:\\JFT-Rapid\\jft.conf").read();
@@ -52,7 +37,7 @@ const gridMenuallyCB = (params: OrgManuallyParams) => {
 };
 // gridMenuallyCB({
 //     mode:"B",
-//     quantity:4,
+//     quantity:3,
 //     sizeContainer:"JFT",
 //     targetSizeChr:"L"
 // })
