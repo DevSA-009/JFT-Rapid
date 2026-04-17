7293e635edcaafc9c9a8bd356b983c9ef08ddf83
DevSA-009 <safin.ali.7205@gmail.com>
2026-04-17 23:52:52 +0600

    refactor: 🚨 host file used stable directive header

--- HOST/script.js
+++ HOST/script.js
@@ -1,4 +1,4 @@
-#target illustrator      // ← Ensures the script targets Illustrator
-#targetengine transient      // Optional but recommended in most cases
+//@target illustrator      // ← Ensures the script targets Illustrator
+//@targetengine "myEngine"      // Optional but recommended in most cases
 
-#include "jft_rapid.jsx"
+//@include "jft_rapid.jsx"
